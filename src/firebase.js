15abc43856351837a127e75f2d851db916f438a5
Gowthamblue <gowtham104050@gmail.com>
2026-02-28 22:20:43 +0530

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl8sMf-IgpnQNrdEHtcYK41QB9jgm3-Gs",
-  authDomain: "exam-portal-168ff.firebaseapp.com",
-  projectId: "exam-portal-168ff",
-  storageBucket: "exam-portal-168ff.firebasestorage.app",
-  messagingSenderId: "58572659518",
-  appId: "1:58572659518:web:7199993e5f3f43ad68ea6f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
